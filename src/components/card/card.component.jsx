23aae9da95dbc90c9466d038962865a237c823d0
mountainguy76935 <stoll33david@gmail.com>
2020-02-04 18:31:58 -0700

    fixed the bug??

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import './card.styles.css';
+import './card-front.styles.css';
 
 export const Card = props => (
-    <div className = 'card_style'>
+    <div className = 'cardfront'>
         <img className="images" alt="photos for site" src={props.picture.src} width="180px" height="180px"/>
         <h2>{props.picture.name}</h2>
         <p>Click Me to learn more!</p>
