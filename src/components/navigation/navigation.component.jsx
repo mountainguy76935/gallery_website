d709a658979a6cd13b42cb80885296339d02c591
mountainguy76935 <stoll33david@gmail.com>
2020-02-21 11:33:38 -0800

    fixed nav bar

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -2,22 +2,44 @@ import React from 'react';
 import './navigation.styles.css';
 import { Link } from 'react-router-dom';
 
-
 export class Navigation extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            toggled: false
+        }
+    }
+
+    handleClick = () => {
+          this.setState({
+              toggled: this.state.toggled === false ? true : false
+          })
+      }
+
     render() {
     let styles = {textDecoration: 'none', color: 'black'};
+    let line = this.state.toggled === true ? 'line' : 'line1';
+    let line1 = this.state.toggled === true ? 'line2' : 'line3';
+    let row = this.state.toggled === true ? 'row' : 'row1';
     return  (
-        <div className = "row">
-            <span className = "hov">
-                <Link style={styles} to="/about">
-                    ABOUT
-                </Link>
-            </span>
-            <span className = "hov">
-                <Link style={styles} strict to="/">
-                    HOME
-                </Link>
-            </span>
+        <div>
+            <div className="block" onClick={this.handleClick}>
+                <div className={line}></div>
+                <div className={line1}></div>
+                <div className={line1}></div>
+            </div>
+            <div className = {row}  name="head" >  
+                <span className="hov">
+                    <Link style={styles} to='/about'>
+                        ABOUT
+                    </Link>
+                </span>
+                <span className="hov">
+                    <Link style={styles} to='/'> 
+                        HOME
+                    </Link> 
+                </span>
+            </div>
         </div>
     )}
 }
