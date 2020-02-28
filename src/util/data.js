e68efee22cea80c5f5e207d1ca3cb07c57043a41
mountainguy76935 <stoll33david@gmail.com>
2020-02-28 10:39:28 -0800

    took some stuff out, added some stuff

--- src/util/data.js
+++ src/util/data.js
@@ -1,21 +1,22 @@
-import logo from '../pictures/IMG_0024.JPG';
 import kayak from '../pictures/kayak.JPG';
 import tips from "../pictures/tips.png";
 import github from "../pictures/github.png";
 import codepen from '../pictures/codepen.jpg';
+import shapes from '../pictures/shapes.jpg';
 
 const data = [
     {
-      name: "Sunset",
+      name: "Shapes",
       id: "img1",
-      src: logo,
-      tagline: "Isn't that just nice??"
+      src: shapes,
+      tagline: "App with shapes! Full screen and click around...",
+      link: "https://polar-plains-71031.herokuapp.com/"
     },
     {
       name: "Kayaking",
       id: "img2",
       src: kayak,
-      tagline: "Looking cool in the ocean"
+      tagline: "This is me, just kayaking and hanging out with a bald eagle"
     },
     {
       name: "Tip Out Calculator",
@@ -24,12 +25,12 @@ const data = [
       tagline: "Just a little tip out calculator that I made for my job...",
       link: 'https://mountainguy76935.github.io/tip_out_calc/'
     },
-    {
-      name: "cat",
-      id: "img4",
-      src: "https://thumbs.dreamstime.com/z/red-cat-16532510.jpg",
-      tagline: "He's a cutie pie with tiny little ears, yeehaw"
-    },
+    // {
+    //   name: "cat",
+    //   id: "img4",
+    //   src: "https://thumbs.dreamstime.com/z/red-cat-16532510.jpg",
+    //   tagline: "He's a cutie pie with tiny little ears, yeehaw"
+    // },
     {
       name: "Github",
       id: "img5",
@@ -44,12 +45,12 @@ const data = [
       tagline: "Click here to check out my tip out calculator, implementing routing, mongoDB, express, and heroku.",
       link: "https://whispering-dawn-27805.herokuapp.com/home"
     },
-    {
-      name: "whales",
-      id: "img7",
-      src: "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555389498/shape/mentalfloss/istock_000046395108_small_0.jpg",
-      tagline: "Our best friends on the planet, and greatest singers"
-    },
+    // {
+    //   name: "whales",
+    //   id: "img7",
+    //   src: "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555389498/shape/mentalfloss/istock_000046395108_small_0.jpg",
+    //   tagline: "Our best friends on the planet, and greatest singers"
+    // },
     {
       name: "code pen",
       id: "img8",
